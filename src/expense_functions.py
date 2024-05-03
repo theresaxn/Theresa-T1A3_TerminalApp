ba6be02051a1203fc880ff8e6b9db1c5a85f6639
theresanx <15274@coderacademy.edu.au>
2024-05-03 22:57:10 +1000

    fixed view feature and added index numbering

--- src/expense_functions.py
+++ src/expense_functions.py
@@ -8,7 +8,12 @@ def add_expense(file_name):
     except ValueError:
         print(f"Invalid date, please try again.")
         date_input = input("Enter date of expense (DD/MM/YYYY): ")
-
+    description_input = input("Enter description of expense: ")
+    amount_input = format(float(input("Enter amount of expense: $")),".2f")
+    
+    with open(file_name, "a") as f:
+        writer = csv.writer(f)
+        writer.writerow([date_expense,description_input,amount_input])
 
 def remove_expense(file_name):
     pass
@@ -18,8 +23,8 @@ def view_expense(file_name):
         with open(file_name, "r") as f:
             reader = csv.reader(f)
             reader.__next__()
-        for row in reader:
-            print(f"Date: {row[0]}, Description: {row[1]}, Amount: ${row[-1]}")
+            for index, row in enumerate(reader):
+                print(f"{index+1}. Date: {row[0]}, Description: {row[1]}, Amount: ${row[2]}")
     except FileNotFoundError:
         print("Expense Tracker file does not exist.")
     except ValueError:
