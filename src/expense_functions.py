8a931d43b625d424360ddeaecaf2f7c54a650a0b
theresanx <15274@coderacademy.edu.au>
2024-05-03 22:28:26 +1000

    imported datetime, created date format in add expense feature

--- src/expense_functions.py
+++ src/expense_functions.py
@@ -1,7 +1,14 @@
 import csv
+import datetime
 
 def add_expense(file_name):
-    pass
+    try:
+        date_input = input("Enter date of expense (DD/MM/YYYY): ")
+        date_expense = datetime.datetime.strptime(date_input,"%d/%m/%Y").strftime("%d-%m-%Y")
+    except ValueError:
+        print(f"Invalid date, please try again.")
+        date_input = input("Enter date of expense (DD/MM/YYYY): ")
+
 
 def remove_expense(file_name):
     pass
@@ -15,7 +22,6 @@ def view_expense(file_name):
             print(f"Date: {row[0]}, Description: {row[1]}, Amount: ${row[-1]}")
     except FileNotFoundError:
         print("Expense Tracker file does not exist.")
-
     except ValueError:
         print("Nothing to view in Expense Tracker.")
 
