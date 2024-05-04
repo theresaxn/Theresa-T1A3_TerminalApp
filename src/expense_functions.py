f6b03cd7349eb74435bcaba39f8426ecbaa10fbe
theresanx <15274@coderacademy.edu.au>
2024-05-04 21:12:59 +1000

    updated add feature, fixed error handling so it loops

--- src/expense_functions.py
+++ src/expense_functions.py
@@ -8,15 +8,11 @@ def add_expense(file_name):
     try:
         date_input = input("Enter date of expense (DD/MM/YYYY): ")
         date_expense = datetime.datetime.strptime(date_input,"%d/%m/%Y").strftime("%d-%m-%Y")
-    except ValueError:
-        print(f"Invalid date, please try again.")
-        date_input = input("Enter date of expense (DD/MM/YYYY): ")
-    description_input = input("Enter description of expense: ")
-    try:
+        description_input = input("Enter description of expense: ")
         amount_input = format(float(input("Enter amount of expense: $")),".2f")
     except ValueError:
-        print("Invalid value, please try again.")
-        amount_input = format(float(input("Enter amount of expense: $")),".2f")
+        print("Invalid value entered, please try again.")
+        add_expense(file_name)
         
     with open(file_name, "a") as f:
         writer = csv.writer(f)
@@ -25,29 +21,45 @@ def add_expense(file_name):
     add_line_break()
 
     add_another_input = input("Add another expense (Y/N)? ")
-
     if add_another_input.lower() == "y":
-            add_expense(file_name)
+        add_expense(file_name)
     elif add_another_input.lower() == "n":
         add_line_break()
     else:
-        print("Invalid response, please try again.")
-        add_another_input = input("Add another expense (Y/N)? ")
+        print("Invalid response, back to main menu.")
+        add_line_break()
     
 def remove_expense(file_name):
-    pass
+    try:
+        line_input = int(input("Enter line number to be deleted: "))
+        expense_lists = []
+    except ValueError:
+        print("Invalid value, please try again.")
+        line_input = int(input("Enter line number to be deleted: "))
 
+    with open(file_name,"r") as f:
+        reader = csv.reader(f)
+        is_exist = False
+        for index, row in enumerate(reader):
+            if (line_input != index):
+                expense_lists.append(row)
+            else:
+                is_exist = True
+    if not is_exist:
+        print("Line number does not exist.")
+    with open(file_name,"w") as f:
+        writer = csv.writer(f)
+        writer.writerows(expense_lists)
+    
 def view_expense(file_name):
     try:
         with open(file_name, "r") as f:
             reader = csv.reader(f)
             reader.__next__()
             for index, row in enumerate(reader):
-                print(f"{index+1}. Date: {row[0]}, Description: {row[1]}, Amount: ${row[2]}")
+                print(f"Line {index+1} - Date: {row[0]}, Description: {row[1]}, Amount: ${row[2]}")
     except FileNotFoundError:
         print("Expense Tracker file does not exist.")
-    except ValueError:
-        print("Nothing to view in Expense Tracker.")
     finally:
         add_line_break()
 
